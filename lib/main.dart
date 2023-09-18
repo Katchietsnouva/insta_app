d1b4d17b16f7948651c06599560a118092b3cb87
Katchietsnouva <philipaswa01@gmail.com>
2023-09-18 16:07:31 +0300

    Functional

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  int currentPage = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
         child: Row(
           children: [
+            const Spacer(),
             IconButton(
-              icon: Icon(Icons.home),
-              onPressed: (){}, 
+              icon: Icon(
+                Icons.home,
+                color: currentPage == 0
+                    ? const Color.fromRGBO(233, 30, 99, 1)
+                    : const Color.fromRGBO(40, 40, 40, 1),
+              ),
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
             ),
+            const Spacer(),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: currentPage == 1
+                    ? const Color.fromRGBO(233, 30, 99, 1)
+                    : const Color.fromRGBO(40, 40, 40, 1),
+              ),
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
+            ),
+            const Spacer(),
+            IconButton(
+              icon: Icon(
+                Icons.ondemand_video,
+                color: currentPage == 1
+                    ? const Color.fromRGBO(233, 30, 99, 1)
+                    : const Color.fromRGBO(40, 40, 40, 1),
+              ),
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
+            ),
+            const Spacer(),
+            IconButton(
+              icon: Icon(
+                Icons.card_travel,
+                color: currentPage == 1
+                    ? const Color.fromRGBO(233, 30, 99, 1)
+                    : const Color.fromRGBO(40, 40, 40, 1),
+              ),
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
+            ),
+            const Spacer(),
+            IconButton(
+              icon: Icon(
+                Icons.person,
+                color: currentPage == 1
+                    ? const Color.fromRGBO(233, 30, 99, 1)
+                    : const Color.fromRGBO(40, 40, 40, 1),
+              ),
+              onPressed: () {
+                setState(() {
+                  currentPage = 0;
+                });
+              },
+            ),
+            const Spacer(),
           ],
         ),
       ),
